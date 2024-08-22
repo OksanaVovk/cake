da5fe33c415e70830b7032f0a23be53db7dd70a0
OksanaVovk <95425405+OksanaVovk@users.noreply.github.com>
2024-08-22 20:25:52 +0000

    Deploying to gh-pages from @ OksanaVovk/cake@d983191baee2c33746c89167f08ce8dc51f4144b 🚀

--- static/js/419.1811a38a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[419],{419:function(e,t,n){n.r(t);var r=n(184);t.default=function(){return(0,r.jsx)("div",{style:{height:"100vh",display:"flex",justifyContent:"center",alignItems:"center",fontSize:40,color:"#010101"},children:(0,r.jsx)("h1",{children:"HELLO UKRAINE"})})}}}]);
-//# sourceMappingURL=419.1811a38a.chunk.js.map
