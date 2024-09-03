3efc60cf1421cbe4dd4d342b47661fc3696ede8a
OksanaVovk <oksvovk85@gmail.com>
2024-09-03 16:21:12 +0300

    Update index.jsx

--- src/components/SharedLayout/index.jsx
+++ src/components/SharedLayout/index.jsx
@@ -9,7 +9,6 @@ export const SharedLayout = () => {
   return (
     <>
       <Header />
-
       {/* <Suspense fallback={<Loader />}> */}
       <Suspense fallback={<div>Loading...</div>}>
         <GlobalContainerStyled>
